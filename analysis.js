a14ca595b4111b81a3bac18434e3b522c8b41579
Darrow <darrowrhartman@gmail.com>
2020-12-22 12:46:12 -0800

    connected tiktok-api init code

--- analysis.js
+++ analysis.js
@@ -1,2 +1,83 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // get tik tok data from api
-console.log('Hello world');
+// * from https://github.com/szdc/tiktok-api
+var tiktok_api_1 = __importStar(require("tiktok-api"));
+// Required - a method that signs the URL with anti-spam parameters
+// You must provide an implementation yourself to successfully make
+// most requests with this library.
+var signURL = function (url, ts, deviceId) { return __awaiter(void 0, void 0, void 0, function () {
+    var as, cp, mas;
+    return __generator(this, function (_a) {
+        as = 'anti-spam parameter 1';
+        cp = 'anti-spam parameter 2';
+        mas = 'anti-spam parameter 3';
+        return [2 /*return*/, url + "&as=" + as + "&cp=" + cp + "&mas=" + mas];
+    });
+}); };
+// Required - device parameters
+// You need to source these using a man-in-the-middle proxy such as mitmproxy,
+// CharlesProxy or PacketCapture (Android)
+var params = tiktok_api_1.getRequestParams({
+    device_id: '<device_id>',
+    fp: '<device_fingerprint>',
+    iid: '<install_id>',
+    openudid: '<device_open_udid>',
+});
+var api = new tiktok_api_1.default(params, { signURL: signURL });
+// You are now able to make successful requests
--- analysis.ts
+++ analysis.ts
@@ -1,3 +1,29 @@
 // get tik tok data from api
+// * from https://github.com/szdc/tiktok-api
+import TikTokAPI, { getRequestParams } from 'tiktok-api';
 
-console.log('Hello world')
+// Required - a method that signs the URL with anti-spam parameters
+// You must provide an implementation yourself to successfully make
+// most requests with this library.
+const signURL = async (url: any, ts: any, deviceId: any) => {
+  const as = 'anti-spam parameter 1';
+  const cp = 'anti-spam parameter 2'
+  const mas = 'anti-spam parameter 3';
+  return `${url}&as=${as}&cp=${cp}&mas=${mas}`;
+}
+
+// Required - device parameters
+// You need to source these using a man-in-the-middle proxy such as mitmproxy,
+// CharlesProxy or PacketCapture (Android)
+const params = getRequestParams({
+  device_id: '<device_id>',
+  fp: '<device_fingerprint>',
+  iid: '<install_id>',
+  openudid: '<device_open_udid>',
+});
+
+const api = new TikTokAPI(params, { signURL });
+
+// You are now able to make successful requests
+
+
